f2244b116ca3115e244630cd2c91c102e6f2a3f3
este6an13 <diquintero@unal.edu.co>
2023-02-15 12:37:18 -0500

    first commit

---
+++ S1C2_02/script.sh
@@ -0,0 +1,10 @@
+mkdir HPC_DiegoEsteban
+cd HPC_DiegoEsteban
+touch loquesea.tex
+cp loquesea.tex loquesea2.tex
+ls
+mv loquesea2.tex loquesea3.tex
+rm loquesea3.tex
+cd ..
+cp -r HPC_DiegoEsteban HPC_DiegoEsteban2
+rm -r HPC_DiegoEsteban
