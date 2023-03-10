5de62d74526f1f1728f4f2c86c0ef7620cef7c42
este6an13 <diquintero@unal.edu.co>
2023-03-10 00:33:27 +0000

    random numbers makefile

--- S5C1_01/QuinteroDiego_HPC_S5C1.mk
+++ S5C1_01/QuinteroDiego_HPC_S5C1.mk
@@ -1,20 +1,18 @@
-CXX=g++
-CXXFLAGS=-Wall -Wextra -pedantic -std=c++17
-LDFLAGS=
+resultados.pdf: resultados.tex histogramas.pdf
+	pdflatex resultados.tex
 
-.PHONY: all clean
+histogramas.pdf: QuinteroDiego_HPC_S5C1.py uniform_1000.dat normal_1000.dat
+	python3 QuinteroDiego_HPC_S5C1.py
 
-all: uniform_1000.exe normal_1000.exe
-    ./uniform_1000.exe > uniform_1000.dat
-    ./normal_1000.exe > normal_1000.dat
-    python QuinteroDiego_HPC_S5C1.py
-    pdflatex resultados.tex
+normal_1000.dat: normal_1000.exe
+	./normal_1000.exe > normal_1000.dat
 
-uniform_1000.exe: uniform_1000.cpp
-    $(CXX) $(CXXFLAGS) -o $@ $< $(LDFLAGS)
+uniform_1000.dat: uniform_1000.exe
+	./uniform_1000.exe > uniform_1000.dat
 
 normal_1000.exe: normal_1000.cpp
-    $(CXX) $(CXXFLAGS) -o $@ $< $(LDFLAGS)
+	g++ normal_1000.cpp -o normal_1000.exe
 
-clean:
-    rm -f *.o *.exe *.dat *.pdf
+uniform_1000.exe: uniform_1000.cpp
+	g++ uniform_1000.cpp -o uniform_1000.exe
+
