8b94600ec92a3c9f64c8b605106f476f0c2c684c
este6an13 <diquintero@unal.edu.co>
2023-05-29 16:58:11 -0500

    monte carlo new plots

--- monte-carlo/plot_times.py
+++ monte-carlo/plot_times.py
@@ -18,18 +18,22 @@ _, efficiency_values = zip(*efficiency)
 
 # Plot speedup
 plt.figure()
-plt.plot(np_values, speedup_values, 'bo-')
+plt.plot(np_values, speedup_values, 'bo-', label='Real Speedup')
+plt.plot(np_values, np_values, '--', color='orange', label='Theretical Speedup')
 plt.xlabel('Number of Processes')
 plt.ylabel('Speedup')
 plt.title('$\pi$ Monte Carlo Speedup for $N=1200000000$')
+plt.legend()
 plt.savefig('S.pdf')
 
 # Plot parallel efficiency
 plt.figure()
 plt.plot(np_values, efficiency_values, 'ro-')
+plt.axhline(y=0.6, color='orange', linestyle='--', label='$60$% Efficiency')
 plt.xlabel('Number of Processes')
 plt.ylabel('Parallel Efficiency')
 plt.title('$\pi$ Monte Carlo Parallel Efficiency for $N=1200000000$')
+plt.legend()
 plt.savefig('E.pdf')
 
 # Show the plots
